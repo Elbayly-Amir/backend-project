35e26af8b16e7be0bebe19be146b36fa5e530c2f
Amir-Elbayly <amirelbayly@gmail.com>
2025-07-02 16:03:29 +0200

    fix: improve database connection logic to handle test environment URLs

--- src/config/database.js
+++ src/config/database.js
@@ -2,13 +2,26 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.DATABASE_URL);
-        console.log('Database connected successfully');
-    } catch (error) {
-        console.error('Database connection failed:', error.message);
-        process.exit(1);
+  const dbUrl = process.env.NODE_ENV === 'test'
+    ? process.env.TEST_DATABASE_URL 
+    : process.env.DATABASE_URL;
+
+  if (!dbUrl) {
+    console.error('Erreur: URL not definned');
+    process.exit(1);
+  }
+
+  try {
+    await mongoose.connect(dbUrl);
+    
+    if (process.env.NODE_ENV !== 'test') {
+      console.log('Database connected successfully');
     }
+
+  } catch (error) {
+    console.error('Database connection failed:', error.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
